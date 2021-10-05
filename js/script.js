8e5d8772108507603df2a5bbdcde2f145082757a
ilyaboy <71150162+Babarica@users.noreply.github.com>
2021-10-05 16:50:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.photo__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="/img/arrow-left.svg"</button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="/img/arrow-right.svg"</button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrow-left.svg"</button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow-right.svg"</button>',
     });
 });
 $(function(){
