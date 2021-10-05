7c7eb8f6663faebe9de6ab7461b919da2a33fe21
ilyaboy <71150162+Babarica@users.noreply.github.com>
2021-10-05 16:06:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.photo__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="/img/arrow-left.svg"</button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/img/arrow-right.svg"</button>',
+    });
+});
+$(function(){
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+});
+});
